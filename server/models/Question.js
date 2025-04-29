f2f74905e87d5c356f8b1ea216af5964ef12142a
ivana <ivana.andrejic21@gmail>
2025-04-29 12:00:49 +0200

    komentari

--- server/models/Question.js
+++ server/models/Question.js
@@ -2,7 +2,6 @@
 const mongoose = require('mongoose');
 
 //Šema za pitanja i odgovore
-
 const questionSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }, 
   question: { type: String, required: true },
@@ -10,7 +9,7 @@ const questionSchema = new mongoose.Schema({
   adminAnswered: { type: Boolean, default: false }, 
   isReadByAdmin: { type: Boolean, default: false }, 
   isReadByUser: { type: Boolean, default: true }, 
-}, { timestamps: true });
+}, { timestamps: true }); //Automatski dodaje polja createdAt i updatedAt u svaki dokument
 
 const Question = mongoose.model('Question', questionSchema);
 
